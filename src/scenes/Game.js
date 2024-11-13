2a26da04b18fedb4e61c555bd166c3fc0b29525c
Endermax <maxouboulenger80@gmail.com>
2024-11-13 18:47:28 +0100

    Ajout de l'animation du player

--- src/scenes/Game.js
+++ src/scenes/Game.js
@@ -17,12 +17,19 @@ export class Game extends Scene
         });  
         this.load.spritesheet('player', 'assets/player.png', {
             frameWidth: 82,  // Largeur d'une frame
-            frameHeight: 160  // Hauteur d'une frame
+            frameHeight: 120  // Hauteur d'une frame
         });   
     }
 
     create ()
     {
+        this.anims.create({
+            key: 'player_idle', // Le nom de l'animation
+            frames: this.anims.generateFrameNumbers('player', { start: 1, end: 3 }), // Frames de l'animation
+            frameRate: 9, // Vitesse de l'animation
+            repeat: -1 // Répéter l'animation en boucle
+        });
+
         this.player = new Player(this, this.scale.width * 0.5, this.scale.height * 0.9, 'player');
 
         this.cameras.main.setBackgroundColor(0x00ff00);
